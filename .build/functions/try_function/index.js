464256654090bd02ce4fa26c56b839f5c7fef431
Crm8-GitHub <crmsession8@gmail.com>
2024-04-07 09:29:10 +0530

    learned to manipulate data from zoho catalyst

--- .build/functions/try_function/index.js
+++ .build/functions/try_function/index.js
@@ -1,17 +1,51 @@
-'use strict';
+const express = require('express');
+const app = express();
+const catalystSDK = require('zcatalyst-sdk-node');
+app.use(express.json());
 
-module.exports = (req, res) => {
-	var url = req.url;
+// Middleware to initialize Catalyst SDK
+app.use((req, res, next) => {
+    const catalyst = catalystSDK.initialize(req);
+    res.locals.catalyst = catalyst;
+    next();
+});
 
-	switch (url) {
-		case '/':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write('<h1>Hello from index.js<h1>');
-			break;
-		default:
-			res.writeHead(404);
-			res.write('You might find the page you are looking for at "/" path');
-			break;
-	}
-	res.end();
-};
+// Route to fetch data from 'Second_Table' and format the response
+app.get('/all', async (req, res) => {
+    try {
+        let { catalyst } = res.locals;
+        let datastore =  catalyst.datastore();
+        let table = datastore.table('Second_Table');
+        let column = await table.getColumnDetails('Row_One');
+        res.status(200).send({
+            status: 'success',
+            data: {
+                column
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            status: 'failure',
+            message: "We're unable to process the request."
+        });
+    }
+});
+app.post('/add',async (req,res)=>{
+    try{
+       let {catalyst} = res.locals;
+       let{Body}  = req.body;
+       let reqBody = req.body;
+       let datastore = catalyst.datastore();
+       let table = datastore.table('Second_Table');
+       let insertRow =await table.insertRow(Body);
+       res.status(200).send({status:'success',data:{reqBody}});
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).send({status:'failure',message:'Unable to Fullfill request'});
+
+    }
+});
+module.exports = app;
--- client/main.js
+++ client/main.js
@@ -1,1 +1,33 @@
-// YOUR JAVASCRIPT CODE FOR INDEX.HTML GOES HERE
+function GetCatalyst() {
+    $.ajax({
+		url: `/server/try_function/all`, //Ensure that 'to_do_list_function' is the package name of your function.
+		success: function (data) {
+			const {data: { column}} = data;
+            
+            console.log(`Catalyst:`, column);
+		},
+		error: function (err) {
+			console.log(err);
+		}
+	});
+
+}
+function InputData() {
+    let inputValue = document.getElementById('ROwONe').value;
+	let Row_Two = document.getElementById('Row_Two').value;
+    let Body = { Row_One: inputValue,Row_Two:Row_Two };
+	// let Body =inputValue;
+    $.ajax({
+        method: 'POST',
+        contentType: 'application/json',
+        url: `/server/try_function/add`,
+        data: JSON.stringify({Body}),
+        success: function (response) {
+            const { data: { reqBody } } = response;
+            console.log(reqBody);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
